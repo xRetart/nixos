4f9a759dd97c9af4d15940a4ba958eb13b48ed44
Richard H <richard.network96@gmail.com>
2024-05-15 20:44:33 +0200

    add lanzaboote

--- flake.nix
+++ flake.nix
@@ -7,10 +7,14 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+	lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.3.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, catppuccin, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, lanzaboote, catppuccin, ... } @ inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -19,7 +23,27 @@
       nixosConfigurations."richard-laptop-nixos" = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = inputs;
-        modules = [ ./configuration.nix ];
+        modules = [
+          ./configuration.nix
+
+          lanzaboote.nixosModules.lanzaboote
+          ({ pkgs, lib, ... }: {
+
+            # For debugging and troubleshooting Secure Boot.
+            environment.systemPackages = [ pkgs.sbctl ];
+
+            # Lanzaboote currently replaces the systemd-boot module.
+            # This setting is usually set to true in configuration.nix
+            # generated at installation time. So we force it to false
+            # for now.
+            boot.loader.systemd-boot.enable = lib.mkForce false;
+
+            boot.lanzaboote = {
+              enable = true;
+              pkiBundle = "/etc/secureboot";
+            };
+          })
+        ];
       };
       homeConfigurations."richard" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
