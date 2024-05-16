f6824babf8292d77ae05eda232e7d295afdb0743
Richard H <richard.network96@gmail.com>
2024-05-16 19:49:09 +0200

    remove lanzaboote

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-	lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.3.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+	# lanzaboote = {
+ #      url = "github:nix-community/lanzaboote/v0.3.0";
+ #      inputs.nixpkgs.follows = "nixpkgs";
+ #    };
     catppuccin.url = "github:catppuccin/nix";
   };
 
@@ -26,23 +26,23 @@
         modules = [
           ./configuration.nix
 
-          lanzaboote.nixosModules.lanzaboote
-          ({ pkgs, lib, ... }: {
-
-            # For debugging and troubleshooting Secure Boot.
-            environment.systemPackages = [ pkgs.sbctl ];
-
-            # Lanzaboote currently replaces the systemd-boot module.
-            # This setting is usually set to true in configuration.nix
-            # generated at installation time. So we force it to false
-            # for now.
-            boot.loader.systemd-boot.enable = lib.mkForce false;
-
-            boot.lanzaboote = {
-              enable = true;
-              pkiBundle = "/etc/secureboot";
-            };
-          })
+          # lanzaboote.nixosModules.lanzaboote
+          # ({ pkgs, lib, ... }: {
+          #
+          #   # For debugging and troubleshooting Secure Boot.
+          #   environment.systemPackages = [ pkgs.sbctl ];
+          #
+          #   # Lanzaboote currently replaces the systemd-boot module.
+          #   # This setting is usually set to true in configuration.nix
+          #   # generated at installation time. So we force it to false
+          #   # for now.
+          #   boot.loader.systemd-boot.enable = lib.mkForce false;
+          #
+          #   boot.lanzaboote = {
+          #     enable = true;
+          #     pkiBundle = "/etc/secureboot";
+          #   };
+          # })
         ];
       };
       homeConfigurations."richard" = home-manager.lib.homeManagerConfiguration {
