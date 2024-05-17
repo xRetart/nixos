ab50c055afdce96d24c030c70e37d3de5fea4087
Richard H <richard.network96@gmail.com>
2024-05-17 22:58:36 +0200

    git aliases

--- home.nix
+++ home.nix
@@ -95,10 +95,16 @@
   programs.firefox.enable = true;
   programs.git = {
     enable = true;
-    extraConfig = {
-      commit = {
-        gpgsign = true;
-      };
+    extraConfig.commit.gpgsign = true;
+    aliases = {
+      a = "add -A";
+      c = "commit -m";
+      d = "diff";
+      l = "log";
+      p = "push";
+      r = "revert";
+      s = "status";
+      ac = "!git a && git c";
     };
   };
   programs.wezterm = {
