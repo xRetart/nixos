24d358e3b81014272365f563a498e494e9edaa43
Richard H <richard.network96@gmail.com>
2024-07-15 23:25:42 +0200

    neovim clippy as rust lsp checker

--- home.nix
+++ home.nix
@@ -37,6 +37,7 @@
       socat
       plocate
       sbctl
+	  chromium
 
       # unfree packages
       spotify
--- programs/neovim/configuration/lua/plugins/rustaceanvim.lua
+++ programs/neovim/configuration/lua/plugins/rustaceanvim.lua
@@ -1,5 +1,18 @@
 return {
 	'mrcjkb/rustaceanvim',
+	config = function()
+		vim.g.rustaceanvim = {
+			server = {
+				default_settings = {
+					['rust-analyzer'] = {
+						check = {
+							command = 'clippy'
+						}
+					}
+				}
+			}
+		}
+	end
 	-- opts = {
 	-- 	server = {
 	-- 		settings = {
