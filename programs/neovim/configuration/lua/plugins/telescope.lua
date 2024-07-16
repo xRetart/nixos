43d839a1cded73a9c721e07272df93b4ce68c244
Richard H <richard.network96@gmail.com>
2024-07-16 16:11:48 +0200

    nvim telescope keybindings

--- programs/neovim/configuration/lua/plugins/telescope.lua
+++ programs/neovim/configuration/lua/plugins/telescope.lua
@@ -7,24 +7,6 @@ return {
 			{ '<leader>pf', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
 			{ '<leader>ps', function() require('telescope.builtin').live_grep() end,  desc = 'Live grep' },
 		},
-		opts = {
-			pickers = {
-				find_files = {
-					mappings = {
-						i = {
-							['<C-e>'] = 'move_selection_previous',
-							-- ['<C-h>'] = 'move_to_top',
-							-- ['<C-m>'] = 'move_to_middle',
-							-- ['<C-l>'] = 'move_to_bottom',
-						},
-						n = {
-							['n'] = 'move_selection_next',
-							['e'] = 'move_selection_previous',
-						},
-					},
-				},
-			},
-		}
 	},
 	{
 		'nvim-telescope/telescope-ui-select.nvim',
