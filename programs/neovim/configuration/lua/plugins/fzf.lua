7da9d2119f62e80477ead09d3de2c7b0aad22605
Richard H <richard.network96@gmail.com>
2024-07-17 17:26:58 +0200

    nvim fzf fix ph

--- programs/neovim/configuration/lua/plugins/fzf.lua
+++ programs/neovim/configuration/lua/plugins/fzf.lua
@@ -2,10 +2,10 @@ return {
 	'ibhagwan/fzf-lua',
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ '<leader>pf', function() require('fzf-lua').files({ cmd = 'fd --type f' }) end, desc = 'Find files' },
-		{ '<leader>pF', function() require('fzf-lua').files() end,                        desc = 'Find files' },
-		{ '<leader>ph', function() require('fzf-lua').files({ cwd_only = true }) end,     desc = 'Find files' },
-		{ '<leader>ps', function() require('fzf-lua').live_grep() end,                    desc = 'Live grep' },
+		{ '<leader>pf', function() require('fzf-lua').files({ cmd = 'fd --type f' }) end,          desc = 'Find files' },
+		{ '<leader>pF', function() require('fzf-lua').files() end,                                 desc = 'Find files' },
+		{ '<leader>ph', function() require('fzf-lua').files({ cwd = vim.fn.expand('%:p:h') }) end, desc = 'Find files' },
+		{ '<leader>ps', function() require('fzf-lua').live_grep() end,                             desc = 'Live grep' },
 	},
 	config = function()
 		require('fzf-lua').register_ui_select()
