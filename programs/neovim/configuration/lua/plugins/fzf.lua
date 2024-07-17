9fd6c3c92f8f12a84b09949eb26685be28288121
Richard H <richard.network96@gmail.com>
2024-07-17 17:09:02 +0200

    nvim fzf dont truncate dirs

--- programs/neovim/configuration/lua/plugins/fzf.lua
+++ programs/neovim/configuration/lua/plugins/fzf.lua
@@ -2,10 +2,10 @@ return {
 	'ibhagwan/fzf-lua',
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ '<leader>pf', function() require('fzf-lua').files({ path_shorten = true, cmd = 'fd --type f' }) end, desc = 'Find files' },
-		{ '<leader>pF', function() require('fzf-lua').files({ path_shorten = true }) end,                      desc = 'Find files' },
-		{ '<leader>ph', function() require('fzf-lua').files({ path_shorten = true, cwd_only = true }) end,     desc = 'Find files' },
-		{ '<leader>ps', function() require('fzf-lua').live_grep() end,                                         desc = 'Live grep' },
+		{ '<leader>pf', function() require('fzf-lua').files({ cmd = 'fd --type f' }) end, desc = 'Find files' },
+		{ '<leader>pF', function() require('fzf-lua').files() end,                        desc = 'Find files' },
+		{ '<leader>ph', function() require('fzf-lua').files({ cwd_only = true }) end,     desc = 'Find files' },
+		{ '<leader>ps', function() require('fzf-lua').live_grep() end,                    desc = 'Live grep' },
 	},
 	config = function()
 		require('fzf-lua').register_ui_select()
