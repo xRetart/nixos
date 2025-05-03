69b07be58642d65a1585bd645232e0af41d037eb
Richard H <richard.network96@gmail.com>
2025-05-03 18:18:01 +0200

    kitty theme dracula

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -3,17 +3,10 @@
 {
   programs.kitty = {
     enable = true;
+	themeFile = "Dracula";
     settings = {
       enable_audio_bell = false;
       confirm_os_window_close = 0;
-    };
-  };
-  xdg.configFile = {
-    "kitty/dracula.conf" = {
-      source = ./config/kitty/dracula.conf;
-    };
-    "kitty/diff.conf" = {
-      source = ./config/kitty/diff.conf;
     };
   };
 }
